76aec20daec3cfeb2d5f4ef70a0195791cdb7ab3
David Yurek <david.a.yurek@gmail.com>
2014-12-26 09:42:31 -0500

    some user attributes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141226140921) do
+ActiveRecord::Schema.define(version: 20141226143509) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,9 @@ ActiveRecord::Schema.define(version: 20141226140921) do
   create_table "users", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
   end
 
 end
